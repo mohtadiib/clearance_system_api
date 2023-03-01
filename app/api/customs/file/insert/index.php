5a529a5b7b13dfd81ca6b51dac9695214ade5266
mohtadyb <mohtadyb@gmail.com>
2023-03-01 18:28:33 +0200

    1-3-2023

--- app/api/customs/file/insert/index.php
+++ app/api/customs/file/insert/index.php
@@ -12,28 +12,28 @@ if(isset($postdata) && !empty($postdata))
     // Extract the data.
     $request = json_decode($postdata,true);
 
-    $table = $request["table"];
     $data = is_assoc($request["data"]);
 
     $containers = json_decode($data[0]["containers"],true);
     $fileData = $data[0]["main"];
-    $products = $data[0]["products"];
+    $products = json_decode($data[0]["products"],true);
+//    $products = $data[0]["products"];
 
     $fileData["file_id"] = $fileId;
 
     $result = insert("files", $fileData, $conn);
     if($result){
-        $products["file_id"] = $fileId;
-
-        $result = insert("file_items", $products, $conn);
-        if($result){
-            foreach ($containers as $container){
-                $container["file_id"] = $fileId;
+        foreach ($containers as $container){
+            $container["file_id"] = $fileId;
 //                $container["id"] = 0;
-                $result = insert("file_containers", $container, $conn);
-            }
-
+            $result = insert("file_containers", $container, $conn);
         }
+            if($result){
+                foreach ($products as $product){
+                    $product["file_id"] = $fileId;
+                    $result = insert("file_items", $product, $conn);
+                }
+            }
 
     }
 
